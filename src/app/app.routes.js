8661bbb170ee74feadc018ec5e5fba5100a54df0
fanich37 <icima.fan@gmail.com>
2021-02-18 00:10:58 +0300

    fix/Fix api url

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -3,8 +3,8 @@ const { app } = require('./app');
 const { UserController } = require('../user');
 const { BooksController } = require('../books');
 
-app.use('/user', UserController);
-app.use('/books', BooksController);
+app.use('/api/user', UserController);
+app.use('/api/books', BooksController);
 app.use('*', (_, res) => res.sendStatus(404));
 
 exports.app = app;
