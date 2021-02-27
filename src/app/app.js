faff67a037af3daf1be2a871e6535b29bbdb939c
fanich37 <icima.fan@gmail.com>
2021-02-27 22:55:24 +0300

    feat/Add ejs; * Rework helmet settings; * Add initial templates

--- src/app/app.js
+++ src/app/app.js
@@ -5,8 +5,22 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(helmet());
+const helmetContentSecurityPolicy = helmet.contentSecurityPolicy.getDefaultDirectives();
+const { 'upgrade-insecure-requests': _, ...rest } = helmetContentSecurityPolicy;
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      ...rest,
+    },
+  },
+}));
 app.use(cors());
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+app.set('views', [
+  path.join(__dirname, '..', 'templates'),
+  path.join(__dirname, '..', 'books', 'templates'),
+]);
+app.set('view engine', 'ejs');
 
 exports.app = app;
