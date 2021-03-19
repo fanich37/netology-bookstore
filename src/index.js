30d9bef059ded3626bf3b25763ee0e362e3aff62
fanich37 <icima.fan@gmail.com>
2021-03-19 20:56:21 +0300

    feat/Add mongoose; * Update docker-compose.dev.yml

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ try {
   // eslint-disable-next-line no-empty
 } catch (error) { }
 
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+const { PORT = 3000, MONGO_DB_CONNECTION } = process.env;
 
 const { app } = require('./app');
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`The server is running on port: ${PORT}.`);
-});
+(async () => {
+  try {
+    await mongoose.connect(MONGO_DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`The server is running on port: ${PORT}.`);
+    });
+  } catch (error) {
+    throw new Error('The error occured while initializing the app...');
+  }
+})();
